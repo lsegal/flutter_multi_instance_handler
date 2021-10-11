541b19f73a3704d8ed69f743786b294a7166a891
tshemeng <tshemeng@users.noreply.github.com>
2021-10-11 10:27:21 +0800

    fix: setMockMethodCallHandler was removed from MethodChannel after upgrading flutter to v2.5.2

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 
